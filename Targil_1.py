df245c29727acf40e6a72195bb00c83020746da0
Yoav <yoav3391@gmail.com>
2019-03-05 13:02:24 +0200

    Fixing variables and functions typo according to PEP 8

--- Targil_1.py
+++ Targil_1.py
@@ -1,16 +1,17 @@
 def main():
     atm = ATM()
     while True:
-        codeNumber = input("Please enter secret code number:")
-        valid = atm.secertCodeCheck(codeNumber)
+        code_number = input("Please enter secret code number:\n")
+        print(" ")
+        valid = atm.secret_code_check(code_number)
         if valid is True:
-            atm.printMenu()
-            customerChoice = int(input())
-            if customerChoice == 1:
-                atm.printAmountOfMoney()
+            atm.print_menu()
+            customer_choice = int(input())
+            if customer_choice == 1:
+                atm.print_amount_of_money()
                 continue
-            elif customerChoice == 2:
-                atm.withdrawMoney()
+            elif customer_choice == 2:
+                atm.withdraw_money()
                 continue
 
         else:
@@ -18,41 +19,38 @@ def main():
 
 
 class ATM:
-    def __init__(self, secretCode = 123456, amountOfMoney = 3000):
-            self._secretCode = secretCode
-            self._amountOfMoney = amountOfMoney
+    def __init__(self, secret_code=123456, amount_of_money=3000):
+            self._secret_code = secret_code
+            self._amount_of_money = amount_of_money
 
-    def secertCodeCheck(self,secretCode):
-            if int(secretCode) == self._secretCode:
+    def secret_code_check(self, secret_code):
+            if int(secret_code) == self._secret_code:
                 return True
             return False
 
-    def printMenu(self):
-        print("{0}\n{1}\n{2}\n{3}\n{4}".format("Welcome to your account customer!",
-                                               "Press 1 to print your account balance",
-                                               "Press 2 to withdraw money from your account",
-                                               "Press 3 to change your secret code number",
-                                               "Press 4 to exit"))
+    def print_menu(self):
+        print("\n{0}\n{1}\n{2}\n{3}\n{4}".format("Welcome to your account customer!",
+                                                 "Press 1 to print your account balance",
+                                                 "Press 2 to withdraw money from your account",
+                                                 "Press 3 to change your secret code number",
+                                                 "Press 4 to exit"))
 
-    def printAmountOfMoney(self):
-            print("{0}{1}".format("Account balance is: ", self._amountOfMoney))
+    def print_amount_of_money(self):
+            print("{0}{1}".format("Account balance is: ", self._amount_of_money))
 
-    def withdrawMoney(self):
-        self.withdrawMoneyMenu()
-        self._customerChoice = int(input())
-        choice = self._customerChoice
-        if choice == 1:
-            self._amountOfMoney -= 20
-        elif choice == 2:
-            self._amountOfMoney -= 50
+    def withdraw_money(self):
+        self.withdraw_money_menu()
+        customer_choice = int(input())
+        if customer_choice == 1:
+            self._amount_of_money -= 20
+        elif customer_choice == 2:
+            self._amount_of_money -= 50
         else:
-            otherAmount = input("Enter other amount to withdraw: ")
-            self._amountOfMoney -= otherAmount
+            other_amount = int(input("Enter other amount to withdraw: "))
+            self._amount_of_money -= other_amount
+        print("Money withdrawal has been made successfully!")
 
-
-
-
-    def withdrawMoneyMenu(self):
+    def withdraw_money_menu(self):
         print("{0}\n{1}\n{2}\n{3}".format("Please choose an amount of money to withdraw:",
                                           "Press 1 to withdraw 20$",
                                           "Press 2 to withdraw 50$",
