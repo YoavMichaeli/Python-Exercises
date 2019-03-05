746dccdcf41bf5a7e81cccfbb1f81812d4f8babd
Yoav <yoav3391@gmail.com>
2019-03-05 13:19:01 +0200

    Adding change secret code function

--- Targil_1.py
+++ Targil_1.py
@@ -1,8 +1,8 @@
 def main():
     atm = ATM()
     while True:
-        code_number = input("Please enter secret code number:\n")
-        print(" ")
+        code_number = input("Welcome to ATM services!\n"
+                            "Please enter secret code number:\n")
         valid = atm.secret_code_check(code_number)
         if valid is True:
             atm.print_menu()
@@ -13,9 +13,12 @@ def main():
             elif customer_choice == 2:
                 atm.withdraw_money()
                 continue
+            elif customer_choice == 3:
+                atm.change_secret_code()
+                continue
 
         else:
-            print("Wrong code number!")
+            print("Wrong code number!\n")
 
 
 class ATM:
@@ -29,14 +32,14 @@ class ATM:
             return False
 
     def print_menu(self):
-        print("\n{0}\n{1}\n{2}\n{3}\n{4}".format("Welcome to your account customer!",
+        print("\n{0}\n{1}\n{2}\n{3}\n{4}".format("Welcome to your customer account!",
                                                  "Press 1 to print your account balance",
                                                  "Press 2 to withdraw money from your account",
                                                  "Press 3 to change your secret code number",
                                                  "Press 4 to exit"))
 
     def print_amount_of_money(self):
-            print("{0}{1}".format("Account balance is: ", self._amount_of_money))
+            print("{0}{1}\n".format("Account balance is: ", self._amount_of_money))
 
     def withdraw_money(self):
         self.withdraw_money_menu()
@@ -48,13 +51,18 @@ class ATM:
         else:
             other_amount = int(input("Enter other amount to withdraw: "))
             self._amount_of_money -= other_amount
-        print("Money withdrawal has been made successfully!")
+        print("Money withdrawal has been made successfully!\n")
 
     def withdraw_money_menu(self):
         print("{0}\n{1}\n{2}\n{3}".format("Please choose an amount of money to withdraw:",
                                           "Press 1 to withdraw 20$",
                                           "Press 2 to withdraw 50$",
                                           "Press 3 to withdraw other amount"))
+
+    def change_secret_code(self):
+        new_code = int(input("Enter new secret code: "))
+        self._secret_code = new_code
+        print("New secret code has been changed successfully!\n")
 
 
 if __name__ == '__main__':
