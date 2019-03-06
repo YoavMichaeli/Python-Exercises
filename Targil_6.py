0df7c102c4fe242347d2864b0bb79131380d6732
Yoav <yoav3391@gmail.com>
2019-03-06 19:30:45 +0200

    Adding notes to Targil_6 functions

--- Targil_6.py
+++ Targil_6.py
@@ -5,11 +5,24 @@ def main():
 
 
 def mapping(f, ls):
+    """
+    The function receives a function and a list and
+    runs the function with the list as an input.
+    :param f:
+    :param ls:
+    :return:
+    """
     for i in range(len(ls)):
         ls[i] = f(ls[i])
 
 
 def add(x):
+    """
+    The function gets as an input a number, add to it 2 and returns
+    the result.
+    :param x:
+    :return: x +2
+    """
     return x + 2
 
 
