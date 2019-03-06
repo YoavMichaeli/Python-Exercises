13b07090c647be6ce5a994b284e8850961f3a9b1
Yoav <yoav3391@gmail.com>
2019-03-06 18:31:07 +0200

    Adding notes to Targil_5 functions

--- Targil_5.py
+++ Targil_5.py
@@ -4,10 +4,18 @@ def main():
 
 
 def check_id(id_num):
+    """
+    The function checks if id_num is a valid number.
+    :param id_num:
+    :return:
+    """
+    # Checks if the id has the right length.
     if valid_id_length(id_num):
+        # Splitting the list and parsing to int.
         digit_list = list(map(int, list(id_num)))
         sum_digits = sum_digits_list(digit_list)
         closest_tenth = round(sum_digits, -1)
+        # Checking if the last digit in the id is correct.
         if abs(closest_tenth - sum_digits) == digit_list[-1]:
             print("The ID is valid!")
             return
@@ -15,10 +23,18 @@ def check_id(id_num):
 
 
 def sum_digits_list(digit_list):
+    """
+    The function calculates the sum of the digits in the list
+    according to the way it needs to be calculate and returns
+    the sum.
+    :param digit_list:
+    :return: sum_digit
+    """
     temp = 2
     mul_by = 1
     sum_digits = 0
     for digit in digit_list[:8]:
+        # Checking if the calculation is not a two digits number
         if len(str(digit * mul_by)) != 1:
             digit = sum(map(int, str(digit * mul_by)))
             sum_digits += digit
@@ -29,6 +45,11 @@ def sum_digits_list(digit_list):
 
 
 def valid_id_length(id_num):
+    """
+    The function checks if id_num length equals 9.
+    :param id_num:
+    :return: False - if id_num doesn't have 9 digits, True - if it has.
+    """
     if len(id_num) != 9:
         return False
     else:
