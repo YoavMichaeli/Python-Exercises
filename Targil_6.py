cb85ef3966e2b79f3254189efe2d83f9d01c2230
Yoav <yoav3391@gmail.com>
2019-03-06 19:25:22 +0200

    Adding add function for testing mapping function

--- Targil_6.py
+++ Targil_6.py
@@ -1,10 +1,16 @@
 def main():
     ls = [1, 2, 3]
+    mapping(add, ls)
+    print(ls)
 
-   
+
 def mapping(f, ls):
     for i in range(len(ls)):
         ls[i] = f(ls[i])
+
+
+def add(x):
+    return x + 2
 
 
 if __name__ == '__main__':
