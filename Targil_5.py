34e404527ff040b521416078017ddc28f070770d
Yoav <yoav3391@gmail.com>
2019-03-06 18:20:12 +0200

    Adding sum digits in list function

--- Targil_5.py
+++ Targil_5.py
@@ -1,9 +1,20 @@
 def main():
     id_number = input("Enter an id with 9 digits please :\n")
-    valid = check_id(id_number)
 
 
 
+def sum_digits_list(digit_list):
+    temp = 2
+    mul_by = 1
+    sum_digits = 0
+    for digit in digit_list[:8]:
+        if len(str(digit * mul_by)) != 1:
+            digit = sum(map(int, str(digit * mul_by)))
+            sum_digits += digit
+        else:
+            sum_digits += digit * mul_by
+        mul_by, temp = temp, mul_by
+    return sum_digits
 
 
 def valid_id_length(id_num):
@@ -11,6 +22,7 @@ def valid_id_length(id_num):
         return False
     else:
         return True
+
 
 if __name__ == '__main__':
     main()
