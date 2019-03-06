331855562f3d0d5c475f07b58669eb76de47a006
Yoav <yoav3391@gmail.com>
2019-03-07 00:21:34 +0200

    Adding decorator cache by creating caching local function

--- Targil_7.py
+++ Targil_7.py
@@ -3,6 +3,16 @@
 def caching_initialize_structure(function):
     cache_stock = dict()
 
+    def caching(*args):
+        if args in cache_stock:
+            return cache_stock[args]
+        else:
+            result = function(*args)
+            cache_stock[args] = result
+            return result
+
+    return caching
+
 
 if __name__ == '__main__':
     main()
