5449b5f7abf388c1a5e44118d9c6e52609b3c256
Yoav <yoav3391@gmail.com>
2019-03-08 11:49:52 +0200

    Adding notes to Targil_1 functions

--- Targil_1.py
+++ Targil_1.py
@@ -1,12 +1,14 @@
 def main():
-    atm = ATM()
+    atm = ATM('1234', 10000)
     start_atm_machine(atm)
 
 
 def start_atm_machine(atm):
     """
     Starting atm machine to serve clients.
-    :param atm:
+    The function operates all atm's functions and repeats the process until the
+    customer wants to quit.
+    :param atm: An ATM object which needs to serve a client.
     :return:
     """
     while True:
@@ -15,6 +17,7 @@ def start_atm_machine(atm):
         valid = atm.secret_code_check(code_number)
         if valid is True:
             atm.print_menu()
+            # Taking customer choice and making his decision.
             customer_choice = input()
             if customer_choice == '1':
                 atm.print_amount_of_money()
@@ -39,36 +42,57 @@ def start_atm_machine(atm):
 
 class ATM:
 
-    def __init__(self, secret_code='1234', amount_of_money=3000):
-            self._secret_code = secret_code
-            self._amount_of_money = amount_of_money
+    def __init__(self, secret_code, amount_of_money):
+        """
+        Initialize function of class ATM
+        :param secret_code: the secret code number of the client(default 1234)
+        :param amount_of_money: the amount of money of the client(default 3000$)
+        """
+        self._secret_code = secret_code
+        self._amount_of_money = amount_of_money
 
-    # Checking if the secret code is valid
     def secret_code_check(self, secret_code):
-            if secret_code == self._secret_code:
-                return True
-            return False
+        """
+        The function checks if the secret code is valid
+        :param secret_code: The user input secret code number
+        :return: True - if there is a match, False - no match.
+        """
+        if secret_code == self._secret_code:
+            return True
+        return False
 
-    # Printing ATM menu
     def print_menu(self):
+        """
+        Printing ATM menu
+        :return:
+        """
         print("\n{0}\n{1}\n{2}\n{3}\n{4}".format("Welcome to your customer account!",
                                                  "Press 1 to print your account balance",
                                                  "Press 2 to withdraw money from your account",
                                                  "Press 3 to change your secret code number",
                                                  "Press 4 to exit"))
 
-    # Printing the amount of money in the current account
     def print_amount_of_money(self):
-            print("{0}{1}\n".format("Account balance is: ", self._amount_of_money))
+        """
+        Printing the amount of money in the current account
+        :return:
+        """
+        print("{0}{1}\n".format("Account balance is: ", self._amount_of_money))
 
-    # Withdraw money from the current account
+    #
     def withdraw_money(self):
+        """
+        Withdraw money from the current account, the client can withdraw
+        20$, 50$ or other amount if he has enough money in the account.
+        :return:
+        """
         current_amount_of_money = self._amount_of_money
         # Checking whether there is money in the account
         if self._amount_of_money == 0:
             print("You don't have money in your account.\n")
             return
         self.withdraw_money_menu()
+        # Taking customer choice checking if it is possible and performing the action.
         customer_choice = input()
         if customer_choice == '1' and self._amount_of_money - 20 >= 0:
             self._amount_of_money -= 20
@@ -78,6 +102,7 @@ class ATM:
             other_amount = int(input("Enter other amount to withdraw: "))
             if self._amount_of_money - other_amount >= 0:
                 self._amount_of_money -= other_amount
+        # Checking if an action has made - means that the transaction made successfully.
         if self._amount_of_money < current_amount_of_money:
             print("Money withdrawal has been made successfully!\n")
         elif customer_choice in ['1', '2', '3']:
@@ -85,15 +110,21 @@ class ATM:
         else:
             print("Wrong input!\n")
 
-    # Printing the withdrawal menu
     def withdraw_money_menu(self):
+        """
+        Printing the withdrawal menu
+        :return:
+        """
         print("{0}\n{1}\n{2}\n{3}".format("Please choose an amount of money to withdraw:",
                                           "Press 1 to withdraw 20$",
                                           "Press 2 to withdraw 50$",
                                           "Press 3 to withdraw other amount"))
 
-    # Changing secret code of the current account
     def change_secret_code(self):
+        """
+        Changing secret code of the current account.
+        :return:
+        """
         new_code = input("Enter new secret code: ")
         valid_flag = self.secret_code_input_check(new_code)
         if valid_flag is False:
@@ -102,11 +133,16 @@ class ATM:
         self._secret_code = new_code
         print("New secret code has been changed successfully!\n")
 
-    # Checking secret code input's validity
     def secret_code_input_check(self, secret_code):
+        """
+        Checking secret code input's validity
+        :param secret_code: The input which received by the client.
+        :return: True - the secret code is valid, False - not valid.
+        """
+        # Checking if the secret code length equals 4
         if len(secret_code) == 4:
-            for code in secret_code:
-                if code.isdigit() is False:
+            # Checking if the secret code contains only digits.
+            if secret_code.isdigit() is False:
                     return False
             return True
         else:
